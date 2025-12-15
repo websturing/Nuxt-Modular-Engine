d7b355a5fdb57c340f8e6043fe3c455bae89f425
websturing <websturing.project@gmail.com>
2025-12-15 11:42:08 +0700

    feat: Implement dynamic page routing from feature folders and restructure the login module.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import fs from 'fs'
+import path from 'path'
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
@@ -64,6 +66,53 @@ export default defineNuxtConfig({
       timezone: process.env.NUXT_PUBLIC_TIMEZONE || 'Asia/Jakarta',
       timeFormat: process.env.NUXT_PUBLIC_TIME_FORMAT || 'HH:mm:ss',
       dateFormat: process.env.NUXT_PUBLIC_DATE_FORMAT || 'YYYY-MM-DD'
+    }
+  },
+  pages: true,
+  hooks: {
+    'pages:extend'(pages) {
+      // [PENTING] 2. Gunakan process.cwd() agar path-nya akurat dari root project
+      const featuresDir = path.resolve(process.cwd(), 'app/features')
+
+      console.log('🏁 START SCANNING FEATURES:', featuresDir) // Log Debug
+
+      if (fs.existsSync(featuresDir)) {
+        fs.readdirSync(featuresDir).forEach((featureName) => {
+          const featurePagesDir = path.resolve(featuresDir, featureName, 'pages')
+
+          if (fs.existsSync(featurePagesDir)) {
+            fs.readdirSync(featurePagesDir).forEach((file) => {
+              if (file.endsWith('.vue')) {
+                const fileNameOriginal = file.replace('.vue', '')
+
+                // Regex: Hapus "Page" atau "Pages" di akhir (Case Insensitive)
+                const cleanName = fileNameOriginal.replace(/Pages?$/i, '').toLowerCase()
+
+                let routePath = ''
+
+                // Logika Routing
+                if (cleanName === 'index' || cleanName === '') {
+                  routePath = `/${featureName}`
+                } else {
+                  routePath = `/${cleanName}`
+                }
+
+                // Push ke Router
+                pages.push({
+                  name: cleanName,
+                  path: routePath,
+                  file: path.resolve(featurePagesDir, file)
+                })
+
+                // [PENTING] 3. Bukti Registrasi
+                console.log(`✅ ROUTE ADDED: ${routePath}  -->  ${featureName}/pages/${file}`)
+              }
+            })
+          }
+        })
+      } else {
+        console.log('⚠️  Folder "features" tidak ditemukan di root!')
+      }
     }
   },
 
